8556f21515434fd9e13791a612882bbf4c8de699
amit <nayakamitkumar272@gmail.com>
2025-04-01 22:35:49 +0530

    assignment 1 completed

---
+++ Week-1-Assignment/Vidyoshah/question6.cpp
@@ -0,0 +1,26 @@
+/*6. Write a C++ program to print the pattern
+sample output:
+for n=3                    for n=2
+123                        12
+112233                     1122
+111222333*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j, k, n;
+    cout<< "enter value of n:";
+    cin>> n;
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n; j++)
+        {
+            for (k = 1; k <= i; k++)
+            {
+                cout << j;
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
