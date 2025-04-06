51b1161747feb81999139387a43c33f6d3ce64c2
Anushree-Mohanty <anushreemohanty938@gmail.com>
2025-04-07 01:28:57 +0530

    added

--- Week-1-Assignment/Anushree-Mohanty_Assignment1/question8.cpp
+++ Week-1-Assignment/Anushree-Mohanty_Assignment1/question8.cpp
@@ -2,26 +2,24 @@
 using namespace std;
 
 void printPattern(int n) {
-    for (int i = 1; i <= n; i++) { // Loop through rows
-        // Print increasing numbers
+    for (int i = 1; i <= n; i++)
+    { 
         for (int j = 1; j <= i; j++) {
             cout << j;
         }
 
-        // Print spaces
+    
         int spaces = (n - i) * 2;
         for (int j = 1; j <= spaces; j++) {
             cout << " ";
         }
 
-        // Print decreasing numbers
+        
         for (int j = i; j >= 1; j--) 
         {
-            // Only print for rows with spaces
                 cout << j;
-        
         }
-        cout << endl; // Move to the next line
+        cout << endl;
     }
 }
 
