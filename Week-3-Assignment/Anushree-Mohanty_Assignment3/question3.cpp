7611340476832a39b164dba36735dfd16ea2302d
Anushree-Mohanty <anushreemohanty938@gmail.com>
2025-04-16 23:28:23 +0530

    added week3 assignment

---
+++ Week-3-Assignment/Anushree-Mohanty_Assignment3/question3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str, result = "";
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    for (int i = 0; i < str.length(); i++) {
+        char ch = str[i];
+        
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+            result += ch;
+        }
+    }
+
+    cout << "String with only alphabets: " << result << endl;
+
+    return 0;
+}
---
+++ Week-3-Assignment/Anushree-Mohanty_Assignment3/question8.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str;
+
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    for (int i = 0; i < str.length() / 2; i++) {
+        if (str[i] != str[str.length() - 1 - i]) {
+            cout << "Not Palindrome" << endl;
+            return 0;
+        }
+    }
+
+    cout << "Palindrome" << endl;
+    return 0;
+}
