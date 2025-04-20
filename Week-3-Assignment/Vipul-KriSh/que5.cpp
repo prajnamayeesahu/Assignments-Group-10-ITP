9e2b78e67c0504edd77b10391eacda935dcb22e4
Vipul-KriSh <vipulkrishnagahir@gmail.com>
2025-04-20 18:33:21 +0530

    Week 3 assignment

---
+++ Week-3-Assignment/Vipul-KriSh/que5.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter size of the array: ";
+    cin >> n;
+    int *arr = new int[n];
+    cout << "Enter  elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int *distinct = new int[n], distinctCount = 0;
+    for (int i = 0; i < n; i++)
+    {
+        bool isDuplicate = false;
+        for (int j = 0; j < distinctCount; j++)
+        {
+            if (arr[i] == distinct[j])
+            {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (!isDuplicate)
+        {
+            distinct[distinctCount] = arr[i];
+            distinctCount++;
+        }
+    }
+    cout << " New Array with distinct elements: ";
+    for (int i = 0; i < distinctCount; i++)
+    {
+        cout << distinct[i] << " ";
+    }
+    cout << endl;
+    delete[] arr;
+    delete[] distinct;
+
+    return 0;
+}
